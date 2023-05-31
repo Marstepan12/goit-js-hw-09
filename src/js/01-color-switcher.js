c097aefed1035989d377fd0c1e5b61e5c6f08ec9
Marstepan12 <121106022+Marstepan12@users.noreply.github.com>
2023-05-31 14:40:23 +0300

    finished

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true;
+});
+
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+});
